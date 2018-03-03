e15dcdc49a2cd96c07f6db3b2cdf5969df37c3cb
Lok Man Christopher Chow <lok.chow@mail.utoronto.ca>
2018-03-03 18:59:22 -0500

    created class

--- spreadsheet.py
+++ spreadsheet.py
@@ -1,46 +1,34 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
- 
- 
 # use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds']
-creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-client = gspread.authorize(creds)
+class Sheet:
+    def __init__(self):
+        scope = ['https://spreadsheets.google.com/feeds']
+        creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+        self.client = gspread.authorize(creds)
+        sheet = self.client.open("Test").sheet1
+        sheet_name = input("Enter your house's name")
+        list_of_sheets = sheet.row_values(1)
+        if sheet_name not in list_of_sheets:
+            new_row = [sheet_name]
+            sheet.insert_row(new_row,1)
+            # Find a workbook by name and open the first sheet
+            # Make sure you use the right name here.
+        self.sheet1 = self.client.open(sheet_name).sheet1
+        self.initialize()
 
-#Number of members and their names
-numOfMembers=0
-numOfMembers = int(input ("How many housemates"))
-numOfMembers = numOfMembers+1
-
-names = {}
-i=2
-while ( i<=numOfMembers):
-    names[i]= input ('Enter name')
-    i=i+1
-
- 
-# Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
-sheet = client.open("Starterhacks").sheet1
- 
+    def initialize(self):
 # Extract and print all of the values
-#sheet.update_cell(3, 1, "I just wrote to a spreadsheet using Python!")
-
-
-#Update their names
-i=2
-while ( i<=numOfMembers):
-    sheet.update_cell(1,i,names[i])
-    sheet.update_cell (i,1,names[i])
-    i=i+1
-
-    
-list_of_hashes = sheet.get_all_records()
-print(list_of_hashes)
-
-
-
-
-
-    
-
+        list_of_hashes = self.sheet1.get_all_records()
+        print(list_of_hashes)
+        numOfMembers=0
+        numOfMembers = int(input ("How many housemates "))
+        i=0
+        while ( i<numOfMembers):
+            sheet.update_cell(1,i+2,names[i])
+            sheet.update_cell (i+2,1,names[i])
+            i=i+1
+        list_of_hashes = sheet.get_all_records()
+        print(list_of_hashes)
+if __name__ == "__main__":
+    sh = Sheet()
